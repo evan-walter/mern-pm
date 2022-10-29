674080f791a9ea46b6dc3d0aa0733705fae889cd
Evan Walter <evan.walter.software@gmail.com>
2022-10-28 20:54:11 -0700

    refactor: clients

--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -1,11 +1,12 @@
 import { useQuery } from '@apollo/client'
 import ClientRow from './ClientRow'
+import Spinner from './Spinner'
 import { GET_CLIENTS } from '../queries/clientQueries'
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS)
 
-  if (loading) return <p>Loading...</p>
+  if (loading) return <Spinner />
   if (error) return <p>Something went wrong.</p>
 
   return (
