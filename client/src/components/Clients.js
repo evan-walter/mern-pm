fb263d3609d7e7d27a10f2d054aba55efcffd774
Evan Walter <evan.walter.software@gmail.com>
2022-10-28 20:27:45 -0700

    refactor: queries

--- client/src/components/Clients.js
+++ client/src/components/Clients.js
@@ -1,16 +1,6 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import ClientRow from './ClientRow'
-
-const GET_CLIENTS = gql`
-  query getClients {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`
+import { GET_CLIENTS } from '../queries/clientQueries'
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS)
