4bf11fbe0836a468036d8bf1136452fbae399065
Evan Walter <evan.walter.software@gmail.com>
2022-10-30 16:05:07 -0700

    display project client data

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,6 +1,7 @@
 import { Link, useParams } from 'react-router-dom'
-import Spinner from '../components/Spinner'
 import { useQuery } from '@apollo/client'
+import Spinner from '../components/Spinner'
+import ClientInfo from '../components/ClientInfo'
 import { GET_PROJECT } from '../queries/projectQueries'
 
 export default function Project() {
@@ -21,6 +22,7 @@ export default function Project() {
           <p>{data.project.description}</p>
           <h5 className='mt-3'>Project Status</h5>
           <p className='lead'>{data.project.status}</p>
+          <ClientInfo client={data.project.client} />
         </div>
       ) : null}
     </>
