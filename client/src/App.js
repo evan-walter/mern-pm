0cf2dbe60222fe3baea0a877fdf455ceaf20a96a
Evan Walter <evan.walter.software@gmail.com>
2022-10-30 15:56:50 -0700

    display project

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Project from './pages/Project'
+import NotFound from './pages/NotFound'
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -35,6 +37,8 @@ export default function App() {
         <div className='container'>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/projects/:id' element={<Project />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
       </Router>
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,23 +1,25 @@
-import {useQuery} from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import Spinner from './Spinner'
 import ProjectCard from './ProjectCard'
-import {GET_PROJECTS} from '../queries/projectQueries'
+import { GET_PROJECTS } from '../queries/projectQueries'
 
 export default function Projects() {
-  const {loading, error,data} = useQuery(GET_PROJECTS)
+  const { loading, error, data } = useQuery(GET_PROJECTS)
 
   if (loading) return <Spinner />
   if (error) return <p>Something went wrong</p>
 
   return (
     <>
-      {data.projects.length > 0?(
+      {data.projects.length > 0 ? (
         <div className='row mt-4'>
-          {data.projects.map(project=>(
-            <ProjectCard key={project.id} project={project}/>
+          {data.projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
           ))}
         </div>
-      ):<p>No Projects</p>}
+      ) : (
+        <p>No Projects</p>
+      )}
     </>
   )
 }
